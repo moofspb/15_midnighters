715c72dc32e9676587cea0772ed3598ac19ecf82
Mikhail Kuntsevich <moofspb@gmail.com>
2017-04-10 18:11:23 +0300

    Add function print_midnighters, remove magic numbers, refactor load_attempts and get_midnighters functions

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,10 +1,12 @@
 import requests
-from datetime import datetime, time
+from datetime import datetime
 from pytz import timezone
 import pytz
 
 
 URL = 'https://devman.org/api/challenges/solution_attempts/'
+MIDNIGHT_HOUR = 0
+MORNING_HOUR = 4
 
 
 def get_pages_count(url):
@@ -13,16 +15,16 @@ def get_pages_count(url):
 
 
 def load_attempts(url):
-    pages = get_pages_count(url)
-    for page in range(pages):
-        request = requests.get(url, {'page': page+1})
+    first_page = requests.get(url)
+    first_page_attempts = first_page.json()['records']
+    pages = first_page.json()['number_of_pages']
+    for attempt in first_page_attempts:
+        yield attempt
+    for page in range(2, pages+1):
+        request = requests.get(url, {'page': page})
         attempts = request.json()['records']
         for attempt in attempts:
-            yield {
-                'username': attempt['username'],
-                'timestamp': attempt['timestamp'],
-                'timezone': attempt['timezone'],
-            }
+            yield attempt
 
 
 def get_attempt_time(attempt):
@@ -37,14 +39,18 @@ def get_midnighters(attempts):
     for attempt in attempts:
         attempt_time = get_attempt_time(attempt)
         if attempt_time:
-            if time(00, 00) < attempt_time.time() < time(4, 00):
+            if MIDNIGHT_HOUR <= attempt_time.hour < MORNING_HOUR:
                 midnighters.add(attempt['username'])
     return midnighters
 
 
-if __name__ == '__main__':
-    attempts = load_attempts(URL)
+def print_midnighters(url):
+    attempts = load_attempts(url)
     midnighters = get_midnighters(attempts)
     print('There is {} midnighters on Devman.org:'.format(len(midnighters)))
     for i, username in enumerate(midnighters, 1):
         print(i, username)
+
+
+if __name__ == '__main__':
+    print_midnighters(URL)
