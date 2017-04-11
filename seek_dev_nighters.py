186bfe7f0c0ba25e57697d4f1cfd0f9281e3c65e
Mikhail Kuntsevich <moofspb@gmail.com>
2017-04-11 14:54:44 +0300

    Refactor function 'print_midnighters', rename variables

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -5,8 +5,8 @@ import pytz
 
 
 URL = 'https://devman.org/api/challenges/solution_attempts/'
-MIDNIGHT_HOUR = 0
-MORNING_HOUR = 4
+FIRST_NIGHT_HOUR = 0
+LAST_NIGHT_HOUR = 3
 
 
 def get_pages_count(url):
@@ -17,10 +17,10 @@ def get_pages_count(url):
 def load_attempts(url):
     first_page = requests.get(url)
     first_page_attempts = first_page.json()['records']
-    pages = first_page.json()['number_of_pages']
+    number_of_pages = first_page.json()['number_of_pages']
     for attempt in first_page_attempts:
         yield attempt
-    for page in range(2, pages+1):
+    for page in range(2, number_of_pages+1):
         request = requests.get(url, {'page': page})
         attempts = request.json()['records']
         for attempt in attempts:
@@ -39,18 +39,18 @@ def get_midnighters(attempts):
     for attempt in attempts:
         attempt_time = get_attempt_time(attempt)
         if attempt_time:
-            if MIDNIGHT_HOUR <= attempt_time.hour < MORNING_HOUR:
+            if FIRST_NIGHT_HOUR <= attempt_time.hour <= LAST_NIGHT_HOUR:
                 midnighters.add(attempt['username'])
     return midnighters
 
 
-def print_midnighters(url):
-    attempts = load_attempts(url)
-    midnighters = get_midnighters(attempts)
+def print_midnighters(midnighters):
     print('There is {} midnighters on Devman.org:'.format(len(midnighters)))
     for i, username in enumerate(midnighters, 1):
         print(i, username)
 
 
 if __name__ == '__main__':
-    print_midnighters(URL)
+    attempts = load_attempts(URL)
+    midnighters = get_midnighters(attempts)
+    print_midnighters(midnighters)
